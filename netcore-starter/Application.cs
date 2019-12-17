7fa8adccea4730e0063895b4e6f205dd5ecb0e44
Chris Opthoog <chris.opthoog@myob.com>
2019-12-18 08:04:23 +1300

    Add project files.

---
+++ netcore-starter/Application.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace netcore_starter
+{
+    public class Application
+    {
+        private readonly ILogger _logger;
+
+        public Application(ILogger<Application> logger)
+        {
+            _logger = logger;
+        }
+
+        internal void Run()
+        {
+            _logger.LogInformation("Application Started at {dateTime}", DateTime.UtcNow);
+
+            // application logic
+
+            _logger.LogInformation("Application Ended at {dateTime}", DateTime.UtcNow);
+        }
+    }
+}
---
+++ netcore-starter/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace netcore_starter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+
+            ConfigureServices(services);
+
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                serviceProvider.GetService<Application>().Run();
+            }
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services
+                .AddLogging(conf => {
+                    conf.ClearProviders();
+                    conf.SetMinimumLevel(LogLevel.Trace);
+                    conf.AddNLog();
+                })
+
+                .AddTransient<Application>()
+
+                ;
+        }
+    }
+}
